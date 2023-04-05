425d7c172e31e318093c5fc02c83daf8214f4e53
codysauermann <80296561+codysauermann@users.noreply.github.com>
2023-04-04 21:09:21 -0400

    Update to plan sweep

--- PlaneSweep.cpp
+++ PlaneSweep.cpp
@@ -2,50 +2,88 @@
 
 //constructor
 PlaneSweep::PlaneSweep() {
-    
+   //
 }
 
 //functions
-void PlaneSweep::add_left(SimplePoint2D dp, Segment2D segment)
+void PlaneSweep::add_left(Segment2D segment)
 {
-            if(sweepStatus.empty())
+    if(sweepStatus.empty())
+    {
+        sweepStatus.push_back(segment); //add half segment to sweep status at beginning
+    }
+    else 
+    {
+        if(segment.leftEndPoint == sweepStatus.back().leftEndPoint) //add half segment to sweep status at end
+        {
+            sweepStatus.push_back(segment);
+            auto itr = sweepStatus.end();
+            segment.findIntersection(*(itr--));
+        }
+        else if(segment.leftEndPoint.x == sweepStatus.back().leftEndPoint.x && segment.leftEndPoint.y > sweepStatus.back().leftEndPoint.y)
+        {
+            sweepStatus.push_back(segment);
+            auto itr = sweepStatus.end();
+            segment.findIntersection(*(itr--));
+        }
+        else //add halfsegment to sweep status somewhere in the middle
+        {
+            int index = sweepStatus.size() - 1;
+            while(!CheckLessThan(segment, sweepStatus[index])) //find index
             {
-                sweepStatus.push_back(segment); //add half segment to sweep status at beginning
+                index -= 1;
             }
-            else if(dp == sweepStatus.back().leftEndPoint) //add half segment to sweep status at end
-            {
-                sweepStatus.push_back(segment);
-            }
-            else if(dp == sweepStatus.back().rightEndPoint) {
-
-            }
-            else //add halfsegment to sweep status somewhere in the middle
-            {
-                int index = sweepStatus.size() - 1;
-                while(!CheckLessThan(GetDominatePoint(currentHalfSeg), sweepStatus[index])) //find index
-                {
-                    index -= 1;
-                }
-                sweepStatus.emplace(sweepStatus.begin() + index, segment); //emplace at index
-            }
+            sweepStatus.emplace(sweepStatus.begin() + index, segment); //emplace at index
+            segment.findIntersection(sweepStatus[index]);
+        } 
+    }   
 }
 
-void PlaneSweep::del_right(SimplePoint2D dp, Segment2D segment)
+void PlaneSweep::del_right(Segment2D segment)
 {
-
+    sweepStatus.erase(std::find(sweepStatus.begin(), sweepStatus.end(), segment));
 }
 
-SimplePoint2D PlaneSweep::get_event()
+SimplePoint2D PlaneSweep::get_event() //Not sure if this is needed
 {
-
+    return currentEventPoint; 
 }
 
-bool PlaneSweep::pred_exists()
+bool PlaneSweep::pred_exists(Segment2D segment)
 {
-
+    if(sweepStatus[0] == segment) 
+    {
+        return false;
+    }
+    else 
+    {
+        return true;
+    }
 }
 
-Segment2D PlaneSweep::pred_of_p() 
+Segment2D PlaneSweep::pred_of_p(Segment2D segment) 
 {
+    //find segment in vector and return that index - 1;
+    auto itr = std::find(sweepStatus.begin(), sweepStatus.end(), segment);
+    itr = itr--;
+    return *itr;
+}
 
+bool CheckLessThan(Segment2D segToAdd, Segment2D prevSeg) 
+{
+    SimplePoint2D dp = segToAdd.leftEndPoint;
+    SimplePoint2D left = prevSeg.leftEndPoint;
+    SimplePoint2D right = prevSeg.rightEndPoint;
+
+    // handle infinite slope
+    if (right.x == left.x) {
+        return dp < left;
+    }
+
+    Number slope = (right.y - left.y) / (right.x - left.x);
+    Number b = left.y - (slope * left.x);   // The minus '-' was originally a '/'. I think this is supposed to be based on y=mx+b, which rewrites into b=y-mx.
+    Number halfSegY = (dp.x * slope) + b;
+
+    return dp.y < halfSegY;
+ 
 }
--- PlaneSweep.h
+++ PlaneSweep.h
@@ -1,21 +1,24 @@
 #include "Segment2D.h"
 #include "SimplePoint2D.h"
 #include <vector>
+#include <queue>
 
 class PlaneSweep {
     protected:
         SimplePoint2D currentEventPoint;
         std::vector<Segment2D> sweepStatus;
+        //possibly keep sweep queues here and implement parallel traversal here
+        //std::queue<SimplePoint2D> 
     public:
         PlaneSweep();
         SimplePoint2D get_event();
         //bool look_ahead();
-        void add_left(SimplePoint2D dp, Segment2D segment);    
-        void del_right(SimplePoint2D dp, Segment2D segment);   
+        void add_left(Segment2D segment);    
+        void del_right(Segment2D segment);   
         //bool get_attr();
         //void set_attr();  
         //bool get_pred_attr(); 
-        bool pred_exists();
-        Segment2D pred_of_p();
+        bool pred_exists(Segment2D segment);
+        Segment2D pred_of_p(Segment2D segment);
         //bool poi_on_seg();  
 };
