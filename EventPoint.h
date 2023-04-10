88d7391fea2bad14523f30abfe83a25343e30a9a
codysauermann <80296561+codysauermann@users.noreply.github.com>
2023-04-10 03:35:43 -0400

    Created event point class for POT functions

---
+++ EventPoint.h
@@ -0,0 +1,10 @@
+#include "HalfSegment2D.h"
+#include "SimplePoint2D.h"
+#include "AttributedHalfSegment2D.h"
+
+struct EventPoint {
+    SimplePoint2D point;
+    HalfSegment2D halfSeg;
+    AttributedHalfSegment2D attrHalfSeg;
+    EventPoint();
+};
