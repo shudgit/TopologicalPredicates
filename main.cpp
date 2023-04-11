19e92c24b171174fcd9f96c29890c2b591ec9804
shudgit <shudouglas@ufl.edu>
2023-04-10 23:43:54 -0400

    Added Disjoint Test to all Pairs

--- main.cpp
+++ main.cpp
@@ -226,7 +226,7 @@ int main ()
         cout << "Point-Line Disjoint Failed Test 3" << endl;
 
     // point-line meets:
-    
+
 
     // point-line equal: N/A
 
@@ -254,26 +254,142 @@ int main ()
     
     // point-line overlaps:
 
+    vector<Segment2D> outer_square;
+    outer_square.push_back(left_long);
+    outer_square.push_back(top_long);
+    outer_square.push_back(right_long);
+    outer_square.push_back(bottom_long);
+    Region2D outer_square_region = Region2D(outer_square);
 
+    Segment2D left_short_lower = left_short;
+    Segment2D left_short_upper = Segment2D(mid_left, top_left);
+    Segment2D top_short_left = Segment2D(top_left, top_mid);
+    Segment2D top_short_right = Segment2D(top_mid, top_right);
+    Segment2D right_short_upper = Segment2D(top_right, mid_right);
+    Segment2D right_short_lower = Segment2D(mid_right, bottom_right);
+    Segment2D bottom_short_right = Segment2D(bottom_mid, bottom_right);
+    Segment2D bottom_short_left = Segment2D(bottom_left, bottom_mid);
+    Segment2D prime_meridian = Segment2D(bottom_mid, top_mid);
+    Segment2D prime_meridian_lower = Segment2D(bottom_mid, mid_mid);
+    Segment2D prime_meridian_upper = Segment2D(mid_mid, top_mid);
+    Segment2D equator = Segment2D(mid_left, mid_right);
+    Segment2D equator_left = Segment2D(mid_left, mid_mid);
+    Segment2D equator_right = Segment2D(mid_mid, mid_right);
+
+    vector<Segment2D> lower_left_square;
+    lower_left_square.push_back(left_short);
+    lower_left_square.push_back(equator_left);
+    lower_left_square.push_back(prime_meridian_lower);
+    lower_left_square.push_back(bottom_short_left);
+    Region2D lower_left_square_region = Region2D(lower_left_square);
+
+    vector<Segment2D> upper_left_square;
+    upper_left_square.push_back(equator_left);
+    upper_left_square.push_back(top_short_left);
+    upper_left_square.push_back(left_short_upper);
+    upper_left_square.push_back(prime_meridian_upper);
+    Region2D upper_left_square_region = Region2D(upper_left_square);
+
+    vector<Segment2D> upper_right_square;
+    upper_right_square.push_back(top_short_right);
+    upper_right_square.push_back(equator_right);
+    upper_right_square.push_back(right_short_upper);
+    upper_right_square.push_back(prime_meridian_upper);
+    Region2D upper_right_square_region = Region2D(upper_right_square);
+
+    vector<Segment2D> lower_right_square;
+    lower_right_square.push_back(equator_right);
+    lower_right_square.push_back(bottom_short_right);
+    lower_right_square.push_back(right_short_lower);
+    lower_right_square.push_back(prime_meridian_lower);
+    Region2D lower_right_square_region = Region2D(lower_right_square);
+
+    vector<Segment2D> lower_rectangle;
+    lower_rectangle.push_back(bottom_long);
+    lower_rectangle.push_back(right_short_lower);
+    lower_rectangle.push_back(left_short_lower);
+    lower_rectangle.push_back(equator);
+    Region2D lower_rectangle_region = Region2D(lower_rectangle);
+
+    vector<Segment2D> upper_rectangle;
+    upper_rectangle.push_back(top_long);
+    upper_rectangle.push_back(left_short_upper);
+    upper_rectangle.push_back(right_short_upper);
+    upper_rectangle.push_back(equator);
+    Region2D upper_rectangle_region = Region2D(upper_rectangle);
+
+    vector<Segment2D> left_rectangle;
+    left_rectangle.push_back(left_long);
+    left_rectangle.push_back(bottom_short_left);
+    left_rectangle.push_back(top_short_left);
+    left_rectangle.push_back(prime_meridian);
+    Region2D left_rectangle_region = Region2D(left_rectangle);
+
+    vector<Segment2D> right_rectangle;
+    right_rectangle.push_back(right_long);
+    right_rectangle.push_back(top_short_right);
+    right_rectangle.push_back(bottom_short_right);
+    right_rectangle.push_back(prime_meridian);
+    Region2D right_rectangle_region = Region2D(right_rectangle);
+
+    Number e = "4.0";
+    SimplePoint2D four_four = SimplePoint2D(e, e);
+    SimplePoint2D three_four = SimplePoint2D(d, e);
+    SimplePoint2D four_three = SimplePoint2D(e, d);
+
+    vector<Segment2D> far_square;
+    far_square.push_back(Segment2D(three_four, four_four));
+    far_square.push_back(Segment2D(four_three, four_four));
+    far_square.push_back(Segment2D(top_right, three_four));
+    far_square.push_back(Segment2D(top_right, four_three));
+    Region2D far_square_region = Region2D(far_square);
 
     // point-region disjoint: 
+    if(vp.disjoint(bottom_points, lower_rectangle_region))
+        cout << "Point-Region Disjoint Failed Test 1" << endl;
+    else
+        cout << "Point-Region Disjoint Passed Test 1" << endl;
+
+    if(vp.disjoint(bottom_points, upper_rectangle_region))
+        cout << "Point-Region Disjoint Passed Test 2" << endl;
+    else
+        cout << "Point-Region Disjoint Failed Test 2" << endl;
+
+    if(vp.disjoint(bottom_points, lower_left_square_region))
+        cout << "Point-Region Disjoint Passed Test 3" << endl;
+    else
+        cout << "Point-Region Disjoint Failed Test 3" << endl;
 
     // point-region meets:
 
-    // point-region equal:
+    // point-region equal: N/A
 
     // point-region inside:
 
-    // point-region covered by:
+    // point-region covered by: N/A
 
-    // point-region contains:
+    // point-region contains: N/A
 
-    // point-region covers:
+    // point-region covers: N/A
     
     // point-region overlaps:
 
 
     // line-line disjoint: 
+    if(vp.disjoint(left_and_bottom_segments, left_and_bottom_segments))
+        cout << "Line-Line Disjoint Failed Test 1" << endl;
+    else
+        cout << "Line-Line Disjoint Passed Test 1" << endl;
+
+    if(vp.disjoint(left_and_bottom_segments, top_and_right_segments))
+        cout << "Line-Line Disjoint Passed Test 2" << endl;
+    else
+        cout << "Line-Line Disjoint Failed Test 2" << endl;
+
+    if(vp.disjoint(left_and_bottom_segments, short_left_and_bottom_segments))
+        cout << "Line-Line Disjoint Passed Test 3" << endl;
+    else
+        cout << "Line-Line Disjoint Failed Test 3" << endl;
 
     // line-line meets:
 
@@ -291,23 +407,51 @@ int main ()
 
 
     // line-region disjoint: 
+    if(vp.disjoint(short_left_and_bottom_segments, lower_rectangle_region))
+        cout << "Line-Region Disjoint Failed Test 1" << endl;
+    else
+        cout << "Line-Region Disjoint Passed Test 1" << endl;
+
+    if(vp.disjoint(right_segments, upper_rectangle_region))
+        cout << "Line-Region Disjoint Passed Test 2" << endl;
+    else
+        cout << "Line-Region Disjoint Failed Test 2" << endl;
+
+    if(vp.disjoint(right_segments, lower_left_square_region))
+        cout << "Line-Region Disjoint Passed Test 3" << endl;
+    else
+        cout << "Line-Region Disjoint Failed Test 3" << endl;
 
     // line-region meets:
 
-    // line-region equal:
+    // line-region equal: N/A
 
     // line-region inside:
 
     // line-region covered by:
 
-    // line-region contains:
+    // line-region contains: N/A
 
-    // line-region covers:
+    // line-region covers: N/A
 
     // line-region overlaps:
 
 
     // region-region disjoint: 
+    if(vp.disjoint(lower_rectangle_region, lower_rectangle_region))
+        cout << "Region-Region Disjoint Failed Test 1" << endl;
+    else
+        cout << "Region-Region Disjoint Passed Test 1" << endl;
+
+    if(vp.disjoint(lower_rectangle_region, upper_rectangle_region))
+        cout << "Region-Region Disjoint Passed Test 2" << endl;
+    else
+        cout << "Region-Region Disjoint Failed Test 2" << endl;
+
+    if(vp.disjoint(lower_left_square_region, far_square_region))
+        cout << "Region-Region Disjoint Passed Test 3" << endl;
+    else
+        cout << "Region-Region Disjoint Failed Test 3" << endl;
 
     // region-region meets:
 
